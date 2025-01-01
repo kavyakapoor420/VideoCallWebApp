1c02144c66a35bdef902c8196e4de71e0f43c834
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-01-01 16:33:40 +0530

    gotMessageFromServer function completed

--- frontend/src/pages/VideMeetComponent/VideoMeeto.jsx
+++ frontend/src/pages/VideMeetComponent/VideoMeeto.jsx
@@ -121,7 +121,25 @@ const VideoMeet=()=>{
     },[video,audio])
      
       let gotMessageFromServer=(fromId,message)=>{
+          var  signal=JSON.parse(message) 
 
+          if(fromId!==socketIdRef.current){
+              if(signal.sdp){
+                connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(()=>{
+                      if(signal.sdp.type==='offer'){
+                          connections[fromId].createAnswer().then((description)=>{
+                              connections[fromId].setLocalDescription(description).then(()=>{
+                                  socketIdRef.current.emit('signal',fromId,JSON.stringify({"sdp":connections[fromId].localDescription}))
+                              }).catch((err)=>console.log(err))
+                          }).catch((e)=>console.log(e))
+                      }
+                }).catch((e)=>console.log(e))
+              } 
+              
+              if(signal.ice){
+                connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice)).catch((e)=>console.log(e))
+              }
+          }
       }
      let addMessage=()=>{
 
